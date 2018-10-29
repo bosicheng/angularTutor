da1da95831c7cf7fccce133c36257207f7f85728
bosicheng <13206650036@163.com>
2018-10-28 23:45:53 -0700

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '九章全栈基础课!';
+  dish_name ='';
+  material_temp='';
+  materials_check='';
+  materials=[];
+  storage=[];
+  showname='';
+  isFalse=false;
+  isTrue=false;
+  match={};
+  isin=true;
+  add(){
+    console.log(this.storage)
+    this.materials = this.material_temp.replace(/\s+/g," ").split(' ');
+    this.match[this.materials] = this.dish_name;
+    for (var i=0;i<this.materials.length;i++){
+      this.storage.push(this.materials[i])
+    }
+    console.log(this.storage);
+  };
+  check(){
+    this.materials = this.materials_check.replace(/\s+/g," ").split(' ');
+    this.isin=true;
+    for(var i=0;i<this.materials.length;i++){
+      if(this.storage.indexOf(this.materials[i]){
+        this.isin=false;
+      })
+    }
+    if (this.isin){
+      this.isTrue=true;
+      this.isFalse=false;
+      this.showname=this.match[this.materials];
+    }
+    else{
+      this.isFalse=true;
+      this.isTrue=false;
+      console.log("false")
+    }
+  }
 }
